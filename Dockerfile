57b2e4076be8dd2c977e8223cdb654eb66513346
Alexey Dmitriev <alexey.dmitriev@ppro.com>
2019-01-29 11:54:54 +0100

    Use multi-stage builds

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
+FROM golang:latest as builder
+RUN mkdir /app
+ADD . /app/
+WORKDIR /app
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
+
 FROM scratch
-ADD main /
+COPY --from=builder /app/main .
 ENV PORT 8080
 EXPOSE 8080
 CMD ["/main"]
--- build.sh
+++ build.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 
-set -e
-
-rm -f main || true
-
-cat > Dockerfile.compile <<EOF
-FROM golang:latest
-RUN mkdir /app
-ADD . /app/
-WORKDIR /app
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
-ENV PORT 8080
-EXPOSE 8080
-CMD ["/app/main"]
-EOF
-docker build -t temp -f Dockerfile.compile .
-rm -f Dockerfile.compile
-docker run --name temp --rm -d temp sleep 60
-docker cp temp:/app/main ./
-docker rm -f temp
-
 IMAGE_PREFIX=dmitrievav
 IMAGE_NAME=hellogo
 TAG=latest
 
 docker build -t $IMAGE_PREFIX/$IMAGE_NAME:$TAG .
-rm -f main
 docker push $IMAGE_PREFIX/$IMAGE_NAME:$TAG
